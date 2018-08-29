954b5dec3b987f091b95c815bb5f1006263c4c8a
MeterSun <zydyouxiang@hotmail.com>
2018-08-29 15:46:09 +0800

    modify

--- make.py
+++ make.py
@@ -6,9 +6,9 @@ import yaml
 import json
 
 # md博客 路径
-postdir = './post/'
+POST_DIR = './post/'
 # 输出目录的路径
-contentpath = './package/content.json'
+CONTENT_PATH = './package/content.json'
 
 # 解析md文件取得有关文件信息的内容
 def parseMarkdownFile(file):
@@ -19,22 +19,28 @@ def parseMarkdownFile(file):
 
 # 将字典类型内容写入json文件
 def createContent(a):
-    with open(contentpath,'w')as f:
+    with open(CONTENT_PATH,'w')as f:
         json.dump(a,f)
 
 def main():
-    content = {}
-    content.update({'all':[]})
-    for filename in os.listdir(postdir):
-        p = parseMarkdownFile(os.path.join(postdir,filename))
-        if not p: continue
-        p['filename'] = filename
-        index = len(content['all']) # index文章位置索引
-        content['all'].append(p)
-        for t in p['tags']:         # 分类记录 t为标签tag
-            if not content.has_key(t): content.update({t:[]})
-            content[t].append(index)
-    createContent(content)
+    Content = {}
+    Content.update({'all':[]})
+    # 遍历 post 目录
+    for filename in os.listdir(POST_DIR):
+        MdFileInfo = parseMarkdownFile(os.path.join(POST_DIR,filename))  # 解析 md 文档
+        if not MdFileInfo: continue
+        # 重命名    
+        newfilename = filename.replace(' ','-').replace('.','_')
+        os.rename(os.path.join(POST_DIR,filename),os.path.join(POST_DIR,newfilename))
+        MdFileInfo.update({'filename':newfilename})
+
+        index = len(Content['all']) # index文章位置索引
+        Content['all'].append(MdFileInfo) # 添加文档信息
+        for tag in MdFileInfo['tags']:         # 分类记录 t为标签tag
+            if not Content.has_key(tag): Content.update({tag:[]})
+            Content[tag].append(index)
+    createContent(Content)
+    print 'all %d files %d tags'%(len(Content['all']),len(Content.keys()))
 
 if __name__ == '__main__':
     main()
