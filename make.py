93545cb1ba7d7eaca498549785e2ff1f96fb136d
MeterSun <zydyouxiang@hotmail.com>
2018-09-18 21:41:10 +0800

    更改时间排序

--- make.py
+++ make.py
@@ -27,20 +27,21 @@ def main():
     Content.update({'all':[]})
     # 遍历 post 目录
     for filename in os.listdir(POST_DIR):
-        MdFileInfo = parseMarkdownFile(os.path.join(POST_DIR,filename))  # 解析 md 文档
-        if not MdFileInfo: continue
+        mdFileInfo = parseMarkdownFile(os.path.join(POST_DIR,filename))  # 解析 md 文档
+        if not mdFileInfo: continue
         # 重命名    
         newfilename = filename.replace(' ','-').replace('.','_')
         os.rename(os.path.join(POST_DIR,filename),os.path.join(POST_DIR,newfilename))
-        MdFileInfo.update({'filename':newfilename})
+        mdFileInfo.update({'filename':newfilename})
+        Content['all'].append(mdFileInfo)  # 添加文档信息
 
-        index = len(Content['all']) # index文章位置索引
-        Content['all'].append(MdFileInfo) # 添加文档信息
-        for tag in MdFileInfo['tags']:         # 分类记录 t为标签tag
-            if not Content.has_key(tag): Content.update({tag:[]})
+    Content['all'].reverse()  # 逆序，最近更新的在最前
+    for index, i in enumerate(Content['all']):
+        for tag in i['tags']:  # 分类记录 t为标签tag
+            if not Content.has_key(tag): Content.update({tag: []})
             Content[tag].append(index)
     createContent(Content)
-    print 'all %d files %d tags'%(len(Content['all']),len(Content.keys()))
+    print 'all %d files %d tags' % (len(Content['all']), len(Content.keys()) - 1)
 
 if __name__ == '__main__':
     main()
